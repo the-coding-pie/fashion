22357fd86a7009cfd3dfd0a6ba782fc28b712d1a
thecodingpie <63698375+the-coding-pie@users.noreply.github.com>
2023-02-20 22:15:10 +0530

    remove font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
-import { Inter } from "@next/font/google";
 import Link from "next/link";
 import Footer from "@/components/Footer/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
